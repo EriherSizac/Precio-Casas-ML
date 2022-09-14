94624426436b85ea25f01d82b2ec6d0844fd4412
Erick Hernández Silva <hernandezsilvaerick@gmail.com>
2022-09-13 19:44:18 -0500

    back

--- spaceship_titanic/Classes/Prediction.py
+++ spaceship_titanic/Classes/Prediction.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 
 class Prediction:
-    def __init__(self, params: [], datosEntrada):
+    def __init__(self, json):
         """
         Metodo que crea una nueva prediccion con los datos enviados
         y guarda los resultados y la informacion en la base de datos.
@@ -22,6 +22,7 @@ class Prediction:
 	    """
 
         import jsonpickle
+        params = self.getParams()
         self.id = None
         self.name = params[0]
         self.email = params[1]
@@ -36,6 +37,47 @@ class Prediction:
         country, form_data, transported) RETURNING id""", (self.name, self.email, self.phone, self.age,
                                                            self.country, datosEntrada, self.transported))
         return self.transported
+
+    def getParams(self, json):
+        params = [
+            json['name'],
+            json['email'],
+            json['phone'],
+            json['age'],
+            json['country'],
+        ]
+        return params
+
+    def genEntryData(self, json):
+
+        datosEntrada = np.array([
+            float(info[0]),
+            float(info[1]),
+            float(info[2]),
+            float(info[3]),
+            float(info[4]),
+            float(info[5]),
+            float(info[6]),
+            float(info[7]),
+            float(info[8]),
+            float(info[9]),
+            float(info[10]),
+            float(info[11]),
+            float(info[12]),
+            float(info[13]),
+            float(info[14]),
+            float(info[15]),
+            float(info[16]),
+            float(info[17]),
+            float(info[18]),
+            float(info[19]),
+            float(info[20]),
+            float(info[21]),
+            float(info[22]),
+            float(info[23]),
+            float(info[24]),
+        ])
+        return datosEntrada
 
     def getReturn(self):
         return self.transported
--- spaceship_titanic/Controllers/Prediction.py
+++ spaceship_titanic/Controllers/Prediction.py
@@ -7,9 +7,7 @@ import numpy as np
 from Global.utils.models import ADABOOST
 def getPrediction():
     json = request.json
-    info = list(json["form_data"].values())
     print(info)
-    params = [json["name"],json["email"],json["phone"],json["age"],json["country"]]
     """
     Index(['cryo_sleep', 'age', 'vip', 'room_service', 'food_court',
        'shopping_mall', 'spa', 'vr_deck', 'transported', 'passenger_group', 
@@ -17,33 +15,6 @@ def getPrediction():
        'S', '55 Cancri e', 'PSO J318.5-22', 'TRAPPIST-1e'],
       dtype='object')"""
 
-    datosEntrada = np.array([
-        float(info[0]),
-        float(info[1]),
-        float(info[2]),
-        float(info[3]),
-        float(info[4]),
-        float(info[5]),
-        float(info[6]),
-        float(info[7]),
-        float(info[8]),
-        float(info[9]),
-        float(info[10]),
-        float(info[11]),
-        float(info[12]),
-        float(info[13]),
-        float(info[14]),
-        float(info[15]),
-        float(info[16]),
-        float(info[17]),
-        float(info[18]),
-        float(info[19]),
-        float(info[20]),
-        float(info[21]),
-        float(info[22]),
-        float(info[23]),
-        float(info[24]),
-    ])
     # Utilizar el modelo
-    prediction = Prediction(params, datosEntrada )
+    prediction = Prediction(json)
     return prediction
