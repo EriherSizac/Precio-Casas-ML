9d91c25f2d8fe2230757d06c990005da28c3c94a
Erick Hernandez Silva <hernandezsilvaerick@gmail.com>
2022-09-13 21:43:58 -0500

    Backend al 100

--- spaceship_titanic/Classes/Prediction.py
+++ spaceship_titanic/Classes/Prediction.py
@@ -4,6 +4,7 @@
 from Global.utils.db import get, post
 from Global.utils.models import ADABOOST
 import pandas as pd
+import numpy as np
 
 
 class Prediction:
@@ -22,7 +23,8 @@ class Prediction:
 	    """
 
         import jsonpickle
-        params = self.getParams()
+        params = self.getParams(json)
+        datosEntrada = self.getEntryData(json)
         self.id = None
         self.name = params[0]
         self.email = params[1]
@@ -31,12 +33,12 @@ class Prediction:
         self.country = params[4]
         self.transported = None
         self.transported = ADABOOST.predict(datosEntrada.reshape(1, -1))
-        print(self.transported)
 
-        id = post("""INSERT INTO User(name, email, phone, age, 
-        country, form_data, transported) RETURNING id""", (self.name, self.email, self.phone, self.age,
-                                                           self.country, datosEntrada, self.transported))
-        return self.transported
+        id = post("""INSERT INTO public."User"(name, email, phone, age, 
+        country, form_data, transported) VALUES(%s, %s, %s, %s, %s, %s, %s) RETURNING id""", (self.name, self.email, self.phone, self.age,
+                                                           self.country, jsonpickle.encode(datosEntrada), bool(self.transported)), returns=True)
+        self.id = id
+
 
     def getParams(self, json):
         params = [
@@ -48,35 +50,61 @@ class Prediction:
         ]
         return params
 
-    def genEntryData(self, json):
+    def getEntryData(self, json):
+        """
+        Index(['cryo_sleep', 'age', 'vip', 'room_service', 'food_court',
+           'shopping_mall', 'spa', 'vr_deck', 'transported', 'passenger_group',
+           'Earth', 'Europa', 'Mars', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'T', 'P',
+           'S', '55 Cancri e', 'PSO J318.5-22', 'TRAPPIST-1e'],
+          dtype='object')"""
+        datosEntrada = [
+            float(json['cryo_sleep']),
+            float(json['age']),
+            float(json['vip']),
+            float(json['room_service']),
+            float(json['food_court']),
+            float(json['shopping_mall']),
+            float(json['spa']),
+            float(json['vr_deck']),
+            float(json['passenger_group']),
+        ]
+        if (json['home_planet']) == 'Earth':
+            datosEntrada.extend([1, 0, 0])
+        elif (json['home_planet']) == 'Europa':
+            datosEntrada.extend([0, 1, 0])
+        elif (json['home_planet']) == 'Mars':
+            datosEntrada.extend([0, 0, 1])
 
-        datosEntrada = np.array([
-            float(info[0]),
-            float(info[1]),
-            float(info[2]),
-            float(info[3]),
-            float(info[4]),
-            float(info[5]),
-            float(info[6]),
-            float(info[7]),
-            float(info[8]),
-            float(info[9]),
-            float(info[10]),
-            float(info[11]),
-            float(info[12]),
-            float(info[13]),
-            float(info[14]),
-            float(info[15]),
-            float(info[16]),
-            float(info[17]),
-            float(info[18]),
-            float(info[19]),
-            float(info[20]),
-            float(info[21]),
-            float(info[22]),
-            float(info[23]),
-            float(info[24]),
-        ])
+        if(json['deck'] == 'A'):
+            datosEntrada.extend([1, 0, 0, 0, 0, 0, 0])
+        elif(json['deck'] == 'B'):
+            datosEntrada.extend([0, 1, 0, 0, 0, 0, 0])
+        elif (json['deck'] == 'C'):
+            datosEntrada.extend([0, 0, 1, 0, 0, 0, 0])
+        elif (json['deck'] == 'D'):
+            datosEntrada.extend([0, 0, 0, 1, 0, 0, 0])
+        elif (json['deck'] == 'E'):
+            datosEntrada.extend([0, 0, 0, 0, 1, 0, 0])
+        elif (json['deck'] == 'F'):
+            datosEntrada.extend([0, 0, 0, 0, 0, 1, 0])
+        elif (json['deck'] == 'G'):
+            datosEntrada.extend([0, 0, 0, 0, 0, 0, 1])
+
+        if json['side'] == 'T':
+            datosEntrada.extend([1, 0, 0])
+        elif json['side'] == 'P':
+            datosEntrada.extend([0, 1, 0])
+        elif json['side'] == 'S':
+            datosEntrada.extend([0, 0, 1])
+
+        if json['destiny'] == '55 Cancri e':
+            datosEntrada.extend([1, 0, 0])
+        elif json['destiny'] == 'PSO J318.5-22':
+            datosEntrada.extend([0, 1, 0])
+        elif json['destiny'] == 'TRAPPIST-1e':
+            datosEntrada.extend([0, 0, 1])
+
+        datosEntrada = np.array(datosEntrada)
         return datosEntrada
 
     def getReturn(self):
--- spaceship_titanic/Controllers/Prediction.py
+++ spaceship_titanic/Controllers/Prediction.py
@@ -7,14 +7,9 @@ import numpy as np
 from Global.utils.models import ADABOOST
 def getPrediction():
     json = request.json
-    print(info)
-    """
-    Index(['cryo_sleep', 'age', 'vip', 'room_service', 'food_court',
-       'shopping_mall', 'spa', 'vr_deck', 'transported', 'passenger_group', 
-       'Earth', 'Europa', 'Mars', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'T', 'P',
-       'S', '55 Cancri e', 'PSO J318.5-22', 'TRAPPIST-1e'],
-      dtype='object')"""
-
     # Utilizar el modelo
     prediction = Prediction(json)
-    return prediction
+    res = {
+        'Transported': bool(prediction.transported)
+    }
+    return res
