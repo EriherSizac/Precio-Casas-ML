99c68845a10d974a9578d151f8228290f6f2a8bd
A01750632 <65176402+A01750632@users.noreply.github.com>
2022-09-15 16:56:21 -0500

    Modelo Update
    Modelo Update

--- Global/utils/models.py
+++ Global/utils/models.py
@@ -3,4 +3,4 @@ from joblib import load
 import os
 # Cargar el modelo
 MODELO_EJERCICIOS = load(os.environ.get('MODELS')+"/modeloArbol.joblib")
-ADABOOST = load(os.environ.get('MODELS')+"/adaboostmodel.joblib")
+ADABOOST = load(os.environ.get('MODELS')+"/XGBoost.joblib")
